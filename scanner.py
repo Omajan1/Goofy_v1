a50afa61098f0f63a459e1fedb41d26a887ab448
Omajan1 <aman.asarfi.0205@gmail.com>
2023-07-13 16:06:53 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -31,7 +31,8 @@ class scanner():
             print(bcolors.FAIL + "[-]" + bcolors.END + " Seems that the host is not responding to ping, trying with -Pn option...")
             scanning_dict = self.scanner.scan(self.ip, "1-65535", "-A -T4")
 
-        print(self.scanner[self.ip]["tcp"].keys())
+        for port in self.scanner[self.ip]["tcp"].keys():
+            print(port)
         # open_ports_list = print(self.scanner[self.ip]["tcp"].keys())
 
         # for port in open_ports_list:
