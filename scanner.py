49b9c6fe02c54bdd143c55d093c2ec7850a432ab
Omajan1 <aman.asarfi.0205@gmail.com>
2023-07-13 15:08:12 +0200

    str update

--- scanner.py
+++ scanner.py
@@ -12,7 +12,7 @@ class scanner():
 
     def start_nmap(self):
         self.scanner.scan(self.ip, "1-65535", "-A -T4")
-        print("[+] IP " + self.ip + " is " + self.scanner[self.ip].state)
+        print("[+] IP " + str(self.ip) + " is " + str(self.scanner[self.ip].state))
 
     def map_scan(self):
         pass
