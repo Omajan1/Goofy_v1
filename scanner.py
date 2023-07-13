5c2ebce2771ec5ba3b22f136114d4c4090130287
Omajan1 <aman.asarfi.0205@gmail.com>
2023-07-13 15:21:35 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,7 +1,9 @@
 import nmap
+from items import bcolors
 
 class scanner():
     scanner = None
+    scanning_dict = {}
 
     def __init__(self, ip):
         self.scanner = nmap.PortScanner()
@@ -11,8 +13,14 @@ class scanner():
         self.start_nmap()
 
     def start_nmap(self):
-        self.scanner.scan(self.ip, "1-65535", "-A -T4")
+        scanning_dict = self.scanner.scan(self.ip, "1-65535", "-A -T4")
         print("[+] IP " + str(self.ip) + " is " + self.scanner[self.ip].state())
+
+        if (self.scanner[self.ip].state() == "down"):
+            print(bcolors.FAIL + "[-]" + bcolors.END + " Seems that the host is not responding to ping, trying with -Pn option...")
+            scanning_dict = self.scanner.scan(self.ip, "1-65535", "-A -T4")
+
+        print(scanning_dict)
 
     def map_scan(self):
         pass
